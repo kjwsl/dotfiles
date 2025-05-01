2307e0e9a08051bcc367000a3db81cc547a44a83
Ray <kjwsl@fatherslab.com>
2025-05-01 21:10:12 +0900

    update(chezmoi): first

---
+++ dot_config/fish/alias.fish
@@ -0,0 +1,16 @@
+alias v='nvim'
+alias v.='nvim .'
+
+alias g='git'
+
+function vf
+    nvim (fzf -m --preview 'bat --style=numbers --color=always {}')
+end
+
+function zf
+    set dir (find . -type d -print | fzf) || return
+    z $dir
+end
+
+
+bind \cf zf
